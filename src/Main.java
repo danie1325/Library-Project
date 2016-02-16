b55f6be4fa6470ad4645cea3be8c1be573e6e75a
Daniella <daniem.13@gmail.com>
2016-02-16 08:53:05 -0800

    updated main

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class Main {
 	//what the fuck am i doing
-	
+
 	public static void main(String args[]){
 
 		//get ISBN #
 		int ISBN = getISBN();
-		
+
 		//figure out if the person is checking out a book or entering one into the database
 		//person is registering a new book in the book database
 		if (getProgramUse() == 0){
@@ -29,18 +29,18 @@ public class Main {
 			else if(getProgramUse() == 1){
 				//figure this shit out later!!
 			}
-			
+
 		}
 
 	}
-	
+
 	public static int getProgramUse(){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Would you like to register a new book (enter 0) or check a book out (enter 1)?");
 		int programUse = input.nextInt();
 		return programUse;
 	}
-	
+
 	public static int getISBN(){
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the book's ISBN number");
@@ -50,4 +50,6 @@ public class Main {
 		int ISBN = Integer.parseInt(ISBNString);
 		return ISBN;
 	}
+
+	
 }
